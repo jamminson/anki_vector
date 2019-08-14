bbe1ede41711be89beedec4c4f953413641b0cfc
Jamin Son <jamin@Jamins-MacBook-Pro.local>
2019-08-14 18:32:37 +0100

    Robot can drive in square

---
+++ motion.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import anki_vector
+import time
+from anki_vector.util import degrees, distance_mm, speed_mmps
+
+
+def main():
+    args = anki_vector.util.parse_command_args()
+    with anki_vector.Robot(args.serial, show_3d_viewer=False, show_viewer=False) as robot:
+        print(robot.pose.position)
+        robot.behavior.drive_off_charger()
+        print(robot.pose.position)
+
+        for i in range(4):
+            print(i)
+            print("Drive Vector straight...")
+            robot.behavior.drive_straight(distance_mm(50), speed_mmps(50))
+
+            print("Turn Vector in place...")
+            robot.behavior.turn_in_place(degrees(90))
+
+        robot.behavior.drive_on_charger()
+
+
+if __name__ == "__main__":
+    main()
