4a11c7a929f4e89f04583abd48b08ee2cda09bf4
Jamin Son <jamin@Jamins-MacBook-Pro.local>
2019-08-15 16:25:13 +0100

    HSV filtering, working on simple blue notebook.

--- color_detection.py
+++ color_detection.py
@@ -9,20 +9,44 @@ import numpy as np
 import tkinter as tk
 import sys
 import anki_vector
+import time
+from anki_vector import audio
+from anki_vector.connection import ControlPriorityLevel
 
 
-args = anki_vector.util.parse_command_args()
-with anki_vector.Robot() as robot:
-    image = robot.camera.capture_single_image()
-    print(f"Displaying image with id {image.image_id}, received at {image.image_recv_time}")
-    image.raw_image.show()
-    raw_image = image.raw_image
-    raw_rgb = np.array(raw_image)
-    hsv_img = cv.cvtColor(raw_rgb, cv.COLOR_RGB2HSV)
-    cv.imshow("raw_image", hsv_img)
-    threshold_image = cv.inRange(hsv_img, (0, 0, 0), (50, 255, 255))
-    cv.imshow("threshold image", threshold_image)
+def main():
+    args = anki_vector.util.parse_command_args()
+    with anki_vector.Robot(args.serial,
+                           default_logging=False,
+                           show_viewer=False,
+                           show_3d_viewer=False,
+                           enable_nav_map_feed=False,
+                           behavior_control_level=ControlPriorityLevel.OVERRIDE_BEHAVIORS_PRIORITY) as robot:
+        robot.audio.set_master_volume(audio.RobotVolumeLevel.LOW)
+
+        while True:
+
+            image = robot.camera.capture_single_image()
+            print(f"Displaying image with id {image.image_id}, received at {image.image_recv_time}")
+            # image.raw_image.show()
+            raw_image = image.raw_image
+            raw_rgb = np.array(raw_image)
+            raw_bgr = cv.cvtColor(raw_rgb, cv.COLOR_RGB2BGR)
+            hsv_img = cv.cvtColor(raw_bgr, cv.COLOR_BGR2HSV)
+            cv.imshow("hsv_image", hsv_img)
+            cv.imshow("raw_image", raw_bgr)
+            threshold_image = cv.inRange(hsv_img, (80, 50, 50), (110, 255, 255))
+            cv.imshow("threshold image", threshold_image)
+            print("1 iteration")
+            cv.waitKey(1)
 
 
+            # try:
+            #     while True:
+            #         time.sleep(0.1)
+            # except KeyboardInterrupt:
+            #     pass
 
 
+if __name__ == "__main__":
+    main()
